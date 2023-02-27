04e4f55b701062918c8f199aec8d8f7d075a3ef9
kasperg3 <kasperg3@rocketmail.com>
2023-02-27 18:50:22 +0100

    experiment runner

--- run_experiments.sh
+++ run_experiments.sh
@@ -13,13 +13,13 @@
 # python3 main.py --dataset=VM25 --route_file_name=mem_r1_route_data0 --experiment_name=ours_8robots_400capacity_repeat30_VM25_13 --n_robots=8 --capacity=350 > ours_8robots_300capacity_repeat30_VM25_13.txt &
 
 
-python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_14robots_infcapacity_AC300 --n_robots=14 --capacity=200 > ours_14robots_infcapacity_AC300.txt
-python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_12robots_infcapacity_AC300 --n_robots=12 --capacity=200 > ours_12robots_infcapacity_AC300.txt
-python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_10robots_infcapacity_AC300 --n_robots=10 --capacity=200 > ours_10robots_infcapacity_AC300.txt
-python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_8robots_infcapacity_AC300 --n_robots=8 --capacity=300 > ours_8robots_infcapacity_AC300.txt
-python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_6robots_infcapacity_AC300 --n_robots=6 --capacity=400 > ours_6robots_infcapacity_AC300.txt
-python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_4robots_infcapacity_AC300 --n_robots=4 --capacity=500 > ours_4robots_infcapacity_AC300.txt
-python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_2robots_infcapacity_AC300 --n_robots=2 --capacity=1000 > ours_2robots_infcapacity_AC300.txt
+python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_14robots_200capacity_AC300 --n_robots=14 --capacity=200 > ours_14robots_infcapacity_AC300.txt
+python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_12robots_200capacity_AC300 --n_robots=12 --capacity=200 > ours_12robots_infcapacity_AC300.txt
+python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_10robots_200capacity_AC300 --n_robots=10 --capacity=200 > ours_10robots_infcapacity_AC300.txt
+python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_8robots_300capacity_AC300 --n_robots=8 --capacity=300 > ours_8robots_infcapacity_AC300.txt
+python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_6robots_400capacity_AC300 --n_robots=6 --capacity=400 > ours_6robots_infcapacity_AC300.txt
+python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_4robots_500capacity_AC300 --n_robots=4 --capacity=500 > ours_4robots_infcapacity_AC300.txt
+python3.11 main.py --dataset=AC300 --route_file_name=mem_inf_route_data0 --experiment_name=trajectory_2robots_1000capacity_AC300 --n_robots=2 --capacity=1000 > ours_2robots_infcapacity_AC300.txt
 
 
 wait
