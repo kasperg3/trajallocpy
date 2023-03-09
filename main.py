7f0c2fc141a61cf300b71f2f482aa39b173e8dc5
Kasper Grøntved <kaspergrontved@gmail.com>
2023-03-09 10:40:29 +0100

    refactored main and using the new datasets

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ import os
 import csv
 import argparse
 
-def findDatasetFiles(dataset, route_file, directory="data/AreaCoverage-dataset/"):
+def getAllCoverageFiles(dataset, directory="data/CoverageTasks/"):
     result = []
     for filename in os.listdir(directory + dataset):
-        filepath = os.path.join(directory + dataset, filename)
-        file_names = []
-        if os.path.isdir(filepath):
-            file_names = Utility.loadDataset(filepath, route_file, "holes", "outer_polygon")
-        result.append(file_names)
+        result.append(os.path.join(directory + dataset, filename))
     return result
 
 
@@ -35,7 +31,6 @@ def saveResults(experiment_title, results):
 
 def main(
     dataset_name,
-    route_filename,
     experiment_title,
     number_of_agents,
     capacity=None,
@@ -45,26 +40,30 @@ def main(
 ):
     seed = 135239
     np.random.seed(seed)
-
+    
     results = []
-    for file_names in files:
-        cp = Utility.loadCoverageProblemFromDict(combined_dict, number_of_agents)
+    files = getAllCoverageFiles(dataset_name)
+
+    for file_name in files:
+        with open(file_name) as json_file:
+            data = json.load(json_file)
+
+        cp = Utility.loadCoverageProblemFromDict(data, number_of_agents)
         exp = Experiment.runner(coverage_problem=cp, enable_plotting=show_plots,max_iterations=200,task_capacity=capacity,use_point_estimation=point_estimation)
         exp.solve(profiling_enabled=False, debug=debug)
 
         # Save the results in a csv file
         (totalRouteLength, sumOfTaskLengths, totalRouteCosts, iterations, computeTime, route_list, maxRouteCost) = exp.evaluateSolution()
-
         results.append(
             [
-                file_names[1].replace("/outer_polygon", ""),
+                file_name,
                 totalRouteLength,
                 sumOfTaskLengths,
                 totalRouteCosts,
                 maxRouteCost,
                 iterations,
                 computeTime,
-                len(task_dict["lines"]),
+                len(data["lines"]),
                 number_of_agents,
             ]
         )
@@ -77,7 +76,6 @@ if __name__ == "__main__":
     # main()
     parser = argparse.ArgumentParser(description="Calculates a conflict free task allocation")
     parser.add_argument("--dataset", type=str, help="The name of the dataset")
-    parser.add_argument("--route_file_name", type=str, help="the filename of the line file")
     parser.add_argument("--experiment_name", type=str, help="The name of the experiment")
     parser.add_argument("--n_robots", type=int, help="The number of robots to include")
     parser.add_argument("--capacity", type=int, help="The capacity of the robots given in minutes")
@@ -91,15 +89,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
     if(len(args._get_args()) != 0):
-        main(
-            args.dataset,
-            args.route_file_name,
-            args.experiment_name,
-            args.n_robots,
-            args.capacity,
-            args.point_estimation,
-            args.show_plots,
-        )
+        main(args.dataset, args.experiment_name, args.n_robots, args.capacity, args.point_estimation, args.show_plots,)
     else: 
-        experiment_title = "AC300_convergence_14robots_test"
-        main( dataset_name="AC300", route_filename="mem_inf_route_data0", experiment_title=experiment_title, number_of_agents=14, capacity=300, point_estimation=False, show_plots=False, debug=False,)
+        ds = "H2"
+        n_agents = 10
+        capacity = 300
+        use_point_est = False
+        main( dataset_name=ds, experiment_title=ds + "_"  + str(n_agents)+ "agents_" +str(capacity)+ "capacity", number_of_agents=14, capacity=300, point_estimation=False, show_plots=False, debug=False,)
