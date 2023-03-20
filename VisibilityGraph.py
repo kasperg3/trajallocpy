829dca9317d54bc3d716f7d9be5f05a08352cd7a
Kasper Grøntved <kaspergrontved@gmail.com>
2023-03-20 13:17:55 +0100

    implemented brute force visibility graph

--- VisibilityGraph.py
+++ VisibilityGraph.py
@@ -31,8 +31,13 @@ def intersection(line1, line2, include_endpoints):
     ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom
     ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom
 
-    if not (0 <= ua <= 1 and 0 <= ub <= 1):  # Not including the endpoints
-        return None  # intersection point is outside of line segments
+    if include_endpoints:
+        if not (0 <= ua <= 1 and 0 <= ub <= 1):  # Not including the endpoints
+            return None  # intersection point is outside of line segments
+    else:
+        if not (0 < ua < 1 and 0 < ub < 1):  # Not including the endpoints
+            return None  # intersection point is outside of line segments
+
     x = x1 + ua * (x2 - x1)
     y = y1 + ua * (y2 - y1)
     return (x, y)
@@ -65,69 +70,25 @@ def does_intersect(seg1, seg2, include_endpoints):
     return False
 
 
-def find_intersection_in_tree(edge, S: BalancedBinarySearchTree):
+def inorder_traversal(current_node):
+    if current_node:
+        inorder_traversal(current_node.left_child)
+        print(current_node)
+        inorder_traversal(current_node.right_child)
+
+
+def search_for_edge(root: Node, key):
+    result = False
+    if root is None or does_intersect(root.edge, key, True):
+        return True
+    else:
+        result = search_for_edge(root.left_child, key) and search_for_edge(root.right_child, key)
+    return result
+
+
+def find_intersection_in_tree(edge, tree: BalancedBinarySearchTree):
     # TODO traverse the tree in order and find if any edges are intersecting!
-    return False
-
-
-def line_intersects_polygon(line, polygon):
-    """
-    Checks if a line intersects the interior of a polygon.
-
-    Parameters:
-    line (tuple): A tuple of two points representing the line.
-    polygon (list): A list of tuples representing the vertices
-                    of the polygon in clockwise or counterclockwise order.
-
-    Returns:
-    bool: True if the line intersects with the interior of the polygon,
-          False otherwise.
-    """
-    # Unpack the line coordinates.
-    x1, y1 = line[0]
-    x2, y2 = line[1]
-
-    # Iterate over the edges of the polygon.
-    for i in range(len(polygon)):
-        # Get the coordinates of the current and next vertex of the polygon.
-        x3, y3 = polygon[i]
-        x4, y4 = polygon[(i + 1) % len(polygon)]
-
-        # Calculate the denominator and numerator of the equations for the line
-        # segments and check for collinearity.
-        denom = ((y4 - y3) * (x2 - x1)) - ((x4 - x3) * (y2 - y1))
-        num1 = ((x4 - x3) * (y1 - y3)) - ((y4 - y3) * (x1 - x3))
-        num2 = ((x2 - x1) * (y1 - y3)) - ((y2 - y1) * (x1 - x3))
-
-        # Check if the line segments are collinear.
-        if denom == 0:
-            if (num1 == 0) and (num2 == 0):
-                # The line segments are coincident.
-                return True
-            else:
-                # The line segments are parallel.
-                continue
-
-        # Calculate the point of intersection of the line segments.
-        t1 = num1 / denom
-        t2 = num2 / denom
-        x_intersect = x1 + t1 * (x2 - x1)
-        y_intersect = y1 + t1 * (y2 - y1)
-
-        # Check if the point of intersection is within the bounds of the line segments
-        # and if it is not one of the endpoints of either line segment.
-        if (
-            (0 < t1 < 1)
-            and (0 < t2 < 1)
-            and ((x_intersect != x1) or (y_intersect != y1))
-            and ((x_intersect != x2) or (y_intersect != y2))
-            and ((x_intersect != x3) or (y_intersect != y3))
-            and ((x_intersect != x4) or (y_intersect != y4))
-        ):
-            # The line segments intersect.
-            return True
-
-    # The line segment does not intersect with the polygon.
+    search_for_edge(tree.root, edge)
     return False
 
 
@@ -167,49 +128,12 @@ def visible_vertices(p, S):
     w_previous = None
     w_previous_visible = None
     for w_i in sorted(S.nodes, key=angle_to_point):
+        T.pretty_print()
         if w_i == p:  # Do not check collision between the same nodes
             continue
 
         p_wi = (p, w_i)
-        visible = True
-
-        # If p_wi intersects any edges of the polygon which w_i is a part of do not check anything else
-        polygon_nodes = list(nx.dfs_preorder_nodes(S, w_i))
-
-        visible = not line_intersects_polygon(p_wi, polygon_nodes)
-
-        # polygon_edges = []
-        # for i in range(len(polygon_nodes)):
-        #     edge = (
-        #         (polygon_nodes[i], polygon_nodes[0])
-        #         if i == len(polygon_nodes) - 1
-        #         else (polygon_nodes[i], polygon_nodes[i + 1])
-        #     )
-        #     polygon_edges.append(edge)
-
-        # # Check for intersections with the nodes own polygon
-        # for edge in polygon_edges:
-        #     # TODO Check if this is correct!
-        #     # do not check the incident edges to w_i
-        #     if not (w_i == edge[0] or w_i == edge[1]):
-        #         continue
-
-        #     if does_intersect(edge, p_wi, include_endpoints=True):
-        #         visible = False
-        #         break
-
-        if visible:
-            # If it is the first iteration or w_i-1 is not on the segment p_wi
-            if w_previous is None or not on_segment(p_wi, w_previous):
-                # search in T for the edge e in the leaftmost leaf
-                e = T.find_min()
-                if e is not None:
-                    visible = False  # not does_intersect(p_wi, e.edge, include_endpoints=False)
-                # visible = not (e is not None and does_intersect(p_wi, e.edge, include_endpoints=True))
-            elif w_previous_visible:
-                # TODO search T for an edge that intersects the segment (w_i-1, w_i)
-                visible = False  # find_intersection_in_tree((w_previous, w_i), S)
-
+        visible = is_visible(S, T, p_wi, w_previous, w_previous_visible, w_i)
         if visible:
             W.append((p, w_i))
             for edge in S.edges(w_i):  # the point have two edges
@@ -231,33 +155,59 @@ def visible_vertices(p, S):
     return W
 
 
+def is_visible(S, T, p_wi, w_previous, w_previous_visible, w_i):
+    visible = True
+
+    # If p_wi intersects any edges of the polygon which w_i is a part of do not check anything else
+    polygon_nodes = list(nx.dfs_preorder_nodes(S, w_i))
+    poly = Polygon(polygon_nodes)
+    line = LineString(p_wi)
+    visible = not line.crosses(poly)
+
+    if visible:
+        # If it is the first iteration or w_i-1 is not on the segment p_wi
+        if w_previous is None or not on_segment(p_wi, w_previous):
+            # search in T for the edge e in the leaftmost leaf
+            e = T.find_min()
+            if e is not None:
+                visible = not does_intersect(p_wi, e.edge, include_endpoints=True)
+        elif w_previous_visible:
+            # TODO search T for an edge that intersects the segment (w_i-1, w_i)
+            e = find_intersection_in_tree((w_previous, w_i), T)
+            if e:
+                visible = False
+        else:
+            visible = False
+    return visible
+
+
 def construct_graph(polygon: Polygon, holes: list):
     G = nx.Graph()
     for i in range(len(polygon.boundary.coords) - 1):
         node_from = polygon.boundary.coords[i]
         node_to = polygon.boundary.coords[i + 1]
-        G.add_node(node_from, pos=node_from)
-        G.add_node(node_to, pos=node_to)
+        G.add_node(node_from, pos=node_from, type="border")
+        G.add_node(node_to, pos=node_to, type="border")
         G.add_edge(polygon.boundary.coords[i], polygon.boundary.coords[i + 1], type="border")
 
     node_from = polygon.boundary.coords[-1]
     node_to = polygon.boundary.coords[len(polygon.boundary.coords) - 2]
-    G.add_node(node_from, pos=node_from)
-    G.add_node(node_to, pos=node_to)
+    G.add_node(node_from, pos=node_from, type="border")
+    G.add_node(node_to, pos=node_to, type="border")
     G.add_edge(polygon.boundary.coords[-1], polygon.boundary.coords[len(polygon.boundary.coords) - 2], type="border")
 
     for hole in holes:
         for i in range(len(hole.boundary.coords) - 1):
             node_from = hole.boundary.coords[i]
             node_to = hole.boundary.coords[i + 1]
-            G.add_node(node_from, pos=node_from)
-            G.add_node(node_to, pos=node_to)
+            G.add_node(node_from, pos=node_from, type="obstacle")
+            G.add_node(node_to, pos=node_to, type="obstacle")
             G.add_edge(hole.boundary.coords[i], hole.boundary.coords[i + 1], type="obstacle")
 
         node_from = hole.boundary.coords[-1]
         node_to = hole.boundary.coords[len(hole.boundary.coords) - 2]
-        G.add_node(node_from, pos=node_from)
-        G.add_node(node_to, pos=node_to)
+        G.add_node(node_from, pos=node_from, type="obstacle")
+        G.add_node(node_to, pos=node_to, type="obstacle")
         G.add_edge(hole.boundary.coords[-1], hole.boundary.coords[len(hole.boundary.coords) - 2], type="obstacle")
     return G
 
@@ -267,7 +217,8 @@ def visibility_graph(polygon: Polygon, holes: list):
     G = construct_graph(polygon, holes)
     # Add all vertices and edges of the polygons to the graph
     print(polygon)
-    print("The polygon ", G)
+    print(holes)
+    print("Polygon as a graph: ", G)
     # check for visibility in each of the vertices
     edges = []
     for v in G.nodes:
@@ -278,12 +229,35 @@ def visibility_graph(polygon: Polygon, holes: list):
     plt.show()
 
 
+def naive_visibility_graph(polygon: Polygon, holes: list):
+    # Create a NetworkX graph to represent the visibility graph
+    visibility_graph = nx.Graph()
+    visibility_graph = construct_graph(polygon, holes)
+
+    for u in visibility_graph.nodes:
+        for v in visibility_graph.nodes:
+            if u == v:
+                continue
+            line = LineString([u, v])
+            intersects_obstacle = False
+            for obstacle in holes:
+                if line.crosses(obstacle) or line.within(obstacle):
+                    intersects_obstacle = True
+                    break
+            if not intersects_obstacle:
+                visibility_graph.add_edge(u, v)
+    print(visibility_graph)
+
+    nx.draw(visibility_graph, nx.get_node_attributes(visibility_graph, "pos"), with_labels=True)
+    plt.show()
+
+
 if __name__ == "__main__":
-    visibility_graph(
-        Polygon([(0, 0), (0, 10), (10, 10), (10, 0)]),
+    naive_visibility_graph(
+        Polygon([(0, 0), (0, 10), (10, 12), (10, 0)]),
         [
             Polygon([(2, 2), (4, 2), (4, 4), (2, 4)]),
-            Polygon([(6, 2), (7, 2), (7, 6), (6, 6)]),
+            Polygon([(6, 2), (7, 2), (7, 7), (6, 7)]),
         ],
     )
     # main()
