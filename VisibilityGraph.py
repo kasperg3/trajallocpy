9ca9967888399d066793e6b96fd01da7cb53506f
Kasper Grøntved <kaspergrontved@gmail.com>
2023-04-18 13:54:00 +0200

    removed old visibility graph impl

--- VisibilityGraph.py
+++ VisibilityGraph.py
@@ -1,4 +1,3 @@
-import json
 import math
 from itertools import combinations
 
@@ -7,190 +6,9 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 from scipy.spatial import KDTree
-from shapely.geometry import (
-    LineString,
-    MultiLineString,
-    MultiPolygon,
-    Point,
-    Polygon,
-    shape,
-)
+from shapely.geometry import LineString, MultiLineString, MultiPolygon, Polygon, shape
 
-from BST import BalancedBinarySearchTree, Node
 from task_allocation import Utility
-
-
-def on_segment(segment, w_i):
-    x, y = w_i
-    (x1, y1), (x2, y2) = segment
-    # check if the point is on the line defined by the segment
-    if (x - x1) * (y2 - y1) == (y - y1) * (x2 - x1):
-        # check if the point is between the endpoints of the segment
-        if (x1 < x < x2 or x2 < x < x1) and (y1 < y < y2 or y2 < y < y1):
-            return True
-    return False
-
-
-def intersection(line1, line2, include_endpoints):
-    x1, y1 = line1[0]
-    x2, y2 = line1[1]
-    x3, y3 = line2[0]
-    x4, y4 = line2[1]
-    denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
-    if denom == 0:
-        return None  # lines are parallel
-    ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom
-    ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom
-
-    if include_endpoints:
-        if not (0 <= ua <= 1 and 0 <= ub <= 1):  # Not including the endpoints
-            return None  # intersection point is outside of line segments
-    else:
-        if not (0 < ua < 1 and 0 < ub < 1):  # Not including the endpoints
-            return None  # intersection point is outside of line segments
-
-    x = x1 + ua * (x2 - x1)
-    y = y1 + ua * (y2 - y1)
-    return (x, y)
-
-
-def does_intersect(seg1, seg2, include_endpoints):
-    x1, y1 = seg1[0]
-    x2, y2 = seg1[1]
-    x3, y3 = seg2[0]
-    x4, y4 = seg2[1]
-
-    dx1 = x2 - x1
-    dy1 = y2 - y1
-    dx2 = x4 - x3
-    dy2 = y4 - y3
-
-    det = dx1 * dy2 - dx2 * dy1
-
-    if det == 0:
-        return False
-
-    t1 = (dx2 * (y1 - y3) - dy2 * (x1 - x3)) / det
-    t2 = (dx1 * (y1 - y3) - dy1 * (x1 - x3)) / det
-    if include_endpoints:
-        if 0 <= t1 <= 1 and 0 <= t2 <= 1:
-            return True
-    else:
-        if 0 < t1 < 1 and 0 < t2 < 1:
-            return True
-    return False
-
-
-def inorder_traversal(current_node):
-    if current_node:
-        inorder_traversal(current_node.left)
-        print(current_node)
-        inorder_traversal(current_node.right)
-
-
-def search_for_edge(root: Node, key):
-    result = True
-    if root is None:
-        return False
-    elif root is not None and does_intersect(root.edge, key, False):
-        return True
-    else:
-        result = search_for_edge(root.left, key) or search_for_edge(root.right, key)
-    return result
-
-
-def find_intersection_in_tree(edge, tree: BalancedBinarySearchTree):
-    return search_for_edge(tree.root, edge)
-
-
-def visible_vertices(p, S):
-    """returns a set of visible vertices from point
-    Parameters
-    ----------
-    holes : Set of obstacles
-    point : a point
-    Returns
-    ----------
-    [(point, visible_vertex, weight), ...]
-
-    """
-    # initialize the BST and result list
-    W = []
-    T = BalancedBinarySearchTree()
-    # sort the obstacle vertices by the angle from initial rho to the vertex
-    # in the case of a draw use the distance to the vertex as a tie breaker
-
-    # Define a custom key function to calculate the angle of each point
-    def angle_to_point(point):
-        x, y = point
-        angle = math.atan2(y - p[1], x - p[0]) * 180 / math.pi
-        distance = math.dist(point, p)
-        return ((360 - angle) % 360, distance)
-
-    # Find the initial intersections and store then in a balanced search tree in the order that they were intersected by the line rho
-    # Initialize the half-line rho to be pointing in the positive x direction
-    p_wi = (p, (p[0] + 10000000, 0))
-    for edge in S.edges():
-        intersection_point = intersection(p_wi, edge, True)
-        if intersection_point is not None:
-            key = (0.0, math.dist(p, intersection_point))
-            T.insert(key, edge)
-
-    # Sort the list of points based on their angle to the origin
-    w_previous = None
-    w_previous_visible = None
-    for w_i in sorted(S.nodes, key=angle_to_point):
-        if w_i == p:  # Do not check collision between the same nodes
-            continue
-
-        p_wi = (p, w_i)
-        visible = is_visible(S, T, p_wi, w_previous, w_previous_visible, w_i)
-        if visible:
-            W.append((p, w_i))
-        # BST upkeep
-        for edge in S.edges(w_i):  # the point have two edges
-            for node in edge:
-                # only compare with the node which is not the common point
-                if node is not w_i:
-                    angle, distance = angle_to_point(node)
-                    if angle_to_point(w_i) <= angle_to_point(node):
-                        # Edge is on the Clockwise side of w_i
-                        T.insert((angle, distance), edge)
-                    else:
-                        # Edge is on the Counter-clockwise side of w_i
-                        T.delete((angle, distance))
-        w_previous = w_i
-        w_previous_visible = visible
-
-    # T.pretty_print()
-
-    return W
-
-
-def is_visible(S, T, p_wi, w_previous, w_previous_visible, w_i):
-    visible = True
-    # If p_wi intersects any edges of the polygon which w_i is a part of do not check anything else
-    polygon_nodes = list(nx.dfs_preorder_nodes(S, w_i))
-    poly = Polygon(polygon_nodes)
-    line = LineString(p_wi)
-    visible = not (line.crosses(poly)) and not line.within(poly)
-
-    if visible:
-        # If it is the first iteration or w_i-1 is not on the segment p_wi
-        if w_previous is None or not on_segment(p_wi, w_previous):
-            # search in T for the edge e in the leaftmost leaf
-            # visible = not find_intersection_in_tree(p_wi, T)
-            # TODO is this the right implementation?
-            e = T.find_min()
-            if e is not None:
-                visible = not does_intersect(p_wi, e.edge, include_endpoints=True)
-        elif w_previous_visible:
-            # search T for an edge that intersects the segment (w_i-1, w_i)
-            # no intersections == visible
-            visible = not find_intersection_in_tree((w_previous, w_i), T)
-        else:
-            visible = False
-    return visible
 
 
 @Utility.timing()
@@ -222,30 +40,6 @@ def construct_graph(polygon: Polygon, holes: MultiPolygon):
             G.add_node(node_from, pos=node_from, type="obstacle")
             G.add_node(node_to, pos=node_to, type="obstacle")
             G.add_edge(hole.boundary.coords[-1], hole.boundary.coords[len(hole.boundary.coords) - 2], type="obstacle")
-    return G
-
-
-@Utility.timing()
-def visibility_graph(polygon: Polygon, holes: list):
-    # Construct the graph
-    G = construct_graph(polygon, holes)
-    # Add all vertices and edges of the polygons to the graph
-    print(polygon)
-    print(holes)
-    print("Polygon as a graph: ", G)
-    # check for visibility in each of the vertices
-    edges = []
-    for v in G.nodes:
-        edges.extend(visible_vertices(v, G))
-
-    G.add_edges_from(edges)
-
-    # Add a cost based on the euclidean distance for each edge
-    nx.set_edge_attributes(
-        G,
-        {e: ((e[0][0] - e[1][0]) ** 2 + (e[0][1] - e[0][1]) ** 2) ** 0.5 for e in G.edges()},
-        "cost",
-    )
     return G
 
 
@@ -380,6 +174,14 @@ def add_points_to_graph(graph, points):
         add_point_to_graph(kdtree, graph, point)
 
 
+def add_edge(graph, point1, point2):
+    if point1 != point2:
+        graph.add_edge(point1, point2)
+    else:
+        pass
+        # print("Trying to create edges to the same point: ", point1, ",", point2)
+
+
 def add_point_to_graph(kdtree, graph, new_point):
     # If the node is already in the graph, do not add it
     if new_point in graph.nodes():
@@ -390,32 +192,34 @@ def add_point_to_graph(kdtree, graph, new_point):
     # Find the nearest edge to the new point
     _, nearest_node_index = kdtree.query(new_point)
     nearest_node = nodes[nearest_node_index]
+    # TODO investigate what happens if the new_point is at the endpoint of a edge
     nearest_edge = min(graph.edges(nearest_node), key=lambda e: point_line_distance(new_point, e))
 
     # Calculate the projection of the new point onto the nearest edge
     endpoint1, endpoint2 = nearest_edge
     projection_point = project_point_onto_line(new_point, endpoint1, endpoint2)
 
+    # Add the new point
     graph.add_node(new_point, pos=new_point)
-    # TODO check whether the new point is on the edge, if to do not add the projected point
 
     # If the projected point is the same point as a existing node, do not add an extra edge
     if projection_point == nearest_node:
-        graph.add_edge(new_point, nearest_node)
+        add_edge(graph, new_point, nearest_node)
     elif projection_point == new_point:
         # point_distance(projection_point, new_point) < 0.1:
         # If the distance from the projection point to the new_point is 0
         graph.remove_edge(endpoint1, endpoint2)
-        graph.add_edge(new_point, endpoint1)
-        graph.add_edge(new_point, endpoint2)
+        add_edge(graph, new_point, endpoint1)
+        add_edge(graph, new_point, endpoint2)
     else:
         # Add the new node and connect it to the projection point and the new point
         # Remove the nearest edge from the graph and add two new edges to the projection point
         graph.remove_edge(endpoint1, endpoint2)
         graph.add_node(projection_point, pos=projection_point)
-        graph.add_edge(new_point, projection_point)
-        graph.add_edge(projection_point, endpoint1)
-        graph.add_edge(projection_point, endpoint2)
+        # TODO sometimes the projectionpoint and endpoint is the same, why is this the case?
+        add_edge(graph, new_point, projection_point)
+        add_edge(graph, projection_point, endpoint2)
+        add_edge(graph, projection_point, endpoint1)
 
 
 def dataset_test():
