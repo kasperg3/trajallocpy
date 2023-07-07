46c2eddba782628d05a910de899f65c40fe36ff2
Kasper Grøntved <kaspergrontved@gmail.com>
2023-07-07 14:42:50 +0200

    return the intersection of the features

--- task-generator/OSMQuery.py
+++ task-generator/OSMQuery.py
@@ -8,28 +8,23 @@ import shapely
 def main():
     # Download the water features data within the bounding box
     tags = {
-        "natural": ["water"],
+        "natural": ["water", "wood", "treerow", "wetland"],
         "landuse": ["farmland", "grassland"],
-        # "highway": ["primary", "secondary", "tertiary", "unclassified", "road"],
+        "highway": ["primary", "secondary", "tertiary", "unclassified", "road"],
     }
 
     polygon = shapely.Polygon([(10.490913, 55.315346), (10.576744, 55.315346), (10.576744, 55.337417), (10.490913, 55.337417)])
     geometries = ox.features_from_polygon(polygon, tags=tags)
+
     # Convert individual polygons to a MultiPolygon
     multi_polygon = geometries.geometry.unary_union
 
-    # TODO group the different categories by type
-
-    # Generate appropriate tasks for each geometry
-
-    #
-
     # Create a GeoDataFrame from the MultiPolygon
-    gdf = gpd.GeoDataFrame(geometry=[multi_polygon], crs=geometries.crs)
+    gdf = gpd.GeoDataFrame(geometry=[shapely.intersection(polygon, multi_polygon)], crs=geometries.crs)
     len(list(gdf.geometry[0].geoms))
     # Plot the lakes
-    ax = gdf.plot(facecolor="blue", edgecolor="black")
-    ax.set_axis_off()
+    ax = gdf.plot(facecolor="white", edgecolor="black")
+    # ax.set_axis_off()
     plt.show()
 
 
