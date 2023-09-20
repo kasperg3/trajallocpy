00a69e91ae9b41debe6f4d4838368532e2e2158c
kasperg3 <kasperg3@rocketmail.com>
2023-09-20 10:53:09 +0200

    improved OSM plotting

--- task-generator/OSMQuery.py
+++ task-generator/OSMQuery.py
@@ -1,16 +1,19 @@
+from functools import partial
+
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import networkx as nx
 import osmnx as ox
 import shapely
+from pyproj import CRS, Transformer
 
 
 def main():
     # Download the water features data within the bounding box
     tags = {
         "natural": ["water", "wood", "treerow", "wetland"],
-        "landuse": ["farmland", "grassland"],
-        "highway": ["primary", "secondary", "tertiary", "unclassified", "road"],
+        # "landuse": ["farmland", "grassland"],
+        # "highway": ["primary", "secondary", "tertiary", "unclassified", "road"],
     }
 
     polygon = shapely.Polygon([(10.490913, 55.315346), (10.576744, 55.315346), (10.576744, 55.337417), (10.490913, 55.337417)])
@@ -21,6 +24,15 @@ def main():
 
     # Create a GeoDataFrame from the MultiPolygon
     gdf = gpd.GeoDataFrame(geometry=[shapely.intersection(polygon, multi_polygon)], crs=geometries.crs)
+    gdf = gdf.to_crs("EPSG:2197")
+    
+    # Get the minimum x and y coordinate values
+    min_x = gdf.bounds['minx'].min()
+    min_y = gdf.bounds['miny'].min()
+    
+    # Apply the normalization to the geometry column of the GeoDataFrame
+    gdf['geometry'] = gdf['geometry'].translate(xoff=-min_x, yoff=-min_y)
+
     len(list(gdf.geometry[0].geoms))
     # Plot the lakes
     ax = gdf.plot(facecolor="white", edgecolor="black")
